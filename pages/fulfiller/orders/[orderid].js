61b710c2099bed7b2a9a1e2836a32e531d72bdd7
Destin Raymundo <draym001@g.ucla.edu>
2020-05-28 22:41:26 -0700

    clean start

--- pages/fulfiller/orders/[orderid].js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-
-export default () => {
-  const router = useRouter();
-  const { orderid } = router.query;
-  return <h1>Hi fulfiller, order status for order: {orderid}</h1>;
-};
