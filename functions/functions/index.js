9b610dd4ced66b110f7c72e6a18237011a1e673e
Destin Raymundo <draym001@g.ucla.edu>
2020-05-31 15:30:41 -0700

    set up functions

---
+++ functions/functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+
+const client = require('twilio')(
+  functions.config().twilio.sid,
+  functions.config().twilio.token
+);
+
+exports.notifyFulfiller = functions.firestore
+  .document('orders/{orderId}')
+  .onCreate(async (snap, context) => {
+    try {
+      const {
+        customer_phone_num,
+        customer_name,
+        order_name,
+        status,
+      } = snap.data();
+
+      const res = await client.messages.create({
+        to: `+1${customer_phone_num}`,
+        from: functions.config().twilio.number,
+        body: `Hi ${customer_name}, your ${order_name}, is ${status}!`,
+      });
+
+      console.log('RESPONSE', res.body);
+      console.log('CONTEXT', context.params.orderId);
+    } catch (err) {
+      console.error('NOTIFY FULFILLER ERROR', err);
+    }
+    return;
+  });
